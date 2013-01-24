d123e71349416fb1d2ea6394b6e58afbca00b499
Mikael Henriksson <mikael@zoolutions.se>
2013-01-24 07:31:32 +0100

    Add missing spec file

---
+++ spec/lib/github/url_spec.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+require 'spec_helper'
+
+module GitHub
+  describe URL do
+    it "generates a username url" do
+      url = GitHub::URL.new(:username, 'KevinSjoberg')
+
+      expected = 'https://github.com/KevinSjoberg'
+      url.get.should eql(expected)
+    end
+
+    it "generates repository url" do
+      url = GitHub::URL.new(:repository, 'KevinSjoberg/github.alfredextension')
+
+      expected = 'https://github.com/KevinSjoberg/github.alfredextension'
+      url.get.should eql(expected)
+    end
+
+    it "generates search url" do
+      url = GitHub::URL.new(:search, 'KevinSjoberg')
+
+      expected = 'https://github.com/search?q=KevinSjoberg'
+      url.get.should eql(expected)
+    end
+
+    it "escapes search query" do
+      url = GitHub::URL.new(:search, 'Kevin Sjöberg')
+      expected = 'https://github.com/search?q=Kevin+Sj%C3%B6berg'
+      url.get.should eql(expected)
+    end
+  end
+end
